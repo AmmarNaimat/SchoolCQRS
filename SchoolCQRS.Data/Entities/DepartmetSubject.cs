75ef83f0bd23d2299960e76a680f8de941331b8f
Ammar Niumat <AmmarNiumat@outlook.com>
2024-07-25 02:04:22 +0300

    add entities

---
+++ SchoolCQRS.Data/Entities/DepartmetSubject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCQRS.Data.Entities
+{
+    public class DepartmetSubject
+    {
+        [Key]
+        public int Id { get; set; }
+        public int DepId { get; set; }
+        public int SubID { get; set; }
+
+        [ForeignKey("DepId")]
+        public virtual Department Department { get; set; }
+
+        [ForeignKey("SubID")]
+        public virtual Subjects Subjects { get; set; }
+    }
+}
---
+++ SchoolCQRS.Data/Entities/StudentSubject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCQRS.Data.Entities
+{
+    public class StudentSubject
+    {
+        [Key]
+        public int Id { get; set; }
+        public int StdId { get; set; }
+        public int SubId { get; set; }
+
+        [ForeignKey("StdId")]
+        public virtual Student Student { get; set; }
+
+        [ForeignKey("SubId")]
+        public virtual Subjects Subject { get; set; }
+    }
+}
